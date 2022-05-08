84141849c662fec317444e06025acd97e6e8f67d
qosndgkisud <100297311+ypuuuuu@users.noreply.github.com>
2022-05-07 20:24:50 -0400

    Delete middlec.py

--- middlec.py
+++
@@ -1,8 +0,0 @@
-# middlec.py
-# This program creates a single SoundWave object that is middle C and saves it to a WAV file called middlec.wav
-
-import soundwave
-
-note = soundwave.SoundWave(0, 2, 1)
-
-note.save("middlec.wav")
